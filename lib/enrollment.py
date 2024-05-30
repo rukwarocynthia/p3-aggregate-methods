cbc6b6269f02384b06a26cc6973362e8ead08491
rukwarocynthia4093@gmail.com <rukwarocynthia4093@gmail.com>
2024-05-31 00:34:36 +0300

    done

--- lib/enrollment.py
+++ lib/enrollment.py
@@ -45,3 +45,10 @@ class Enrollment:
 
     def get_enrollment_date(self):
         return self._enrollment_date
+    @classmethod
+    def aggregate_enrollments_per_day(cls):
+        enrollment_count = {}
+        for enrollment in cls.all:
+            date = enrollment.get_enrollment_date().date()
+            enrollment_count[date] = enrollment_count.get(date, 0) + 1
+        return enrollment_count
